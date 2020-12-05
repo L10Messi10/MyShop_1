e3baefefcdcfea0dead4356d1ee317846b6c1759
L10Messi10 <loveyourenemy@outlook.com>
2020-12-05 15:01:14 +0800

    Add project files.

---
+++ MyShop_1.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop_1.WebUI.Startup))]
+namespace MyShop_1.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
